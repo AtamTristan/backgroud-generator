01d4a266bc7a41fb2ab696165f92d4b08580e042
AtamTristan <tristan.wesendahl@gmail.com>
2023-06-15 23:43:03 +0200

    create random color button and suiting colors after loading

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var button = document.querySelector("button");
 
 function setGradient() {
 	body.style.background = 
@@ -14,6 +15,31 @@ function setGradient() {
 	css.textContent = body.style.background + ";";
 }
 
+function randomColor(){
+	// Generate a random integer between 1 and 255
+	const randomNumber = Math.ceil(Math.random() * 16777216) -1;
+	let randomHex = randomNumber.toString(16);
+	while(randomHex.length < 6){
+		randomHex = "0".concat("", randomHex);
+	}
+	randomHex = "#".concat("", randomHex);
+
+	return randomHex;
+} 
+
+function randomColors(){
+	color1.value = randomColor();
+	color2.value = randomColor();
+	setGradient();
+}
+
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+button.addEventListener("click", randomColors);
+
+setGradient();
+
+
+
